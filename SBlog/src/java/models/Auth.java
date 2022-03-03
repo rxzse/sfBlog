2f0cdeb8fe826d38cef4e50b0ed1f2d3d0038375
Thai V <rxztek@gmail.com>
2022-03-03 10:09:27 +0700

    model auth add

--- SBlog/src/java/models/Auth.java
+++ SBlog/src/java/models/Auth.java
@@ -4,11 +4,54 @@
  * and open the template in the editor.
  */
 package models;
-
+import java.util.Date;
 /**
  *
  * @author RxZ
  */
 public class Auth {
     // password, createTime, modifyTime
+    private String password;
+    private Date createTime, modifyTime;
+
+    public Auth() {
+    }
+
+    public Auth(String password, Date createTime, Date modifyTime) {
+        this.password = password;
+        this.createTime = createTime;
+        this.modifyTime = modifyTime;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Auth{" + "password=" + password + ", createTime=" + createTime + ", modifyTime=" + modifyTime + '}';
+    }
+    
+    
+    
 }
