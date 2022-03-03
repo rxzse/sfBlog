33fb21071af56f1e387e03a78bee5ef7951edf3b
Thai V <rxztek@gmail.com>
2022-03-03 10:13:18 +0700

    settings

--- SBlog/src/java/models/Setting.java
+++ SBlog/src/java/models/Setting.java
@@ -11,4 +11,45 @@ package models;
  */
 public class Setting {
     // siteName, siteAuthor, baseUrl
+    private String siteName, siteAuthor, baseUrl;
+
+    public Setting() {
+    }
+
+    public Setting(String siteName, String siteAuthor, String baseUrl) {
+        this.siteName = siteName;
+        this.siteAuthor = siteAuthor;
+        this.baseUrl = baseUrl;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getSiteAuthor() {
+        return siteAuthor;
+    }
+
+    public void setSiteAuthor(String siteAuthor) {
+        this.siteAuthor = siteAuthor;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Setting{" + "siteName=" + siteName + ", siteAuthor=" + siteAuthor + ", baseUrl=" + baseUrl + '}';
+    }
+    
+    
 }
