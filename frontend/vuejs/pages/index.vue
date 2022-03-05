41b4331446420e61c28029ee178b73535ae043c3
rxztek <rxztek@gmail.com>
2022-03-05 19:34:07 +0700

    test add css

---
+++ frontend/vuejs/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <h2 class="title"># Kho hàng</h2>
+  <itable />
+  </div>
+</template>
+
+<script>
+import iTable from "~/components/itable.vue";
+
+export default {
+  name: "IndexPage",
+  components: {
+    iTable,
+    // Card
+  },
+};
+</script>
