2ec457fc5ba2e7b847ba0c6f16f99ba9c321047b
rxztek <rxztek@gmail.com>
2022-03-13 14:30:51 +0700

    build admin proxy

---
+++ zFix/sBlog/src/main/java/db/AdminDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.time.LocalDateTime;
+import java.sql.Date;
+import java.time.LocalDate;
+
+import models.Post;
+import models.Category;
+
+/**
+ *
+ * @author RxZ
+ */
+public class AdminDAO {
+    private static DBContext db = new DBContext();
+    
+    
+}
